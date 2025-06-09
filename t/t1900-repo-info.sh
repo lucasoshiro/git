b1e251a6634e17e3718c2c74fcfec0feb45c71be
Lucas Seiki Oshiro <lucasseikioshiro@gmail.com>
2025-06-08 23:21:11 -0300

    repo-info: add the field references.format
    This commit is part of the series that introduce the new command
    git-repo-info.
    
    The flag `--show-ref-format` from git-rev-parse is used for retrieving
    the reference format (i.e. `files` or `reftable`). This way, it is
    used for querying repository information, fitting in the purpose of
    git-repo-info.
    
    Then, add a new field `references.format` to the repo-info command
    containing that information.
    
    Helped-by: Phillip Wood <phillip.wood@dunelm.org.uk>
    Helped-by: Junio C Hamano <gitster@pobox.com>
    Mentored-by: Karthik Nayak <karthik.188@gmail.com>
    Mentored-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Lucas Seiki Oshiro <lucasseikioshiro@gmail.com>

---
+++ t/t1900-repo-info.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+test_description='test git repo-info'
+
+. ./test-lib.sh
+
+parse_json () {
+	tr '\n' ' ' | "$PERL_PATH" "$TEST_DIRECTORY/t0019/parse_json.perl"
+}
+
+test_lazy_prereq PERLJSON '
+	perl -MJSON -e "exit 0"
+'
+
+# Test if a field is correctly returned in both null-terminated and json formats.
+#
+# Usage: test_repo_info <label> <init command> <key> <expected value>
+#
+# Arguments:
+#   label: the label of the test
+#   init command: a command that creates a repository called 'repo', configured
+#      accordingly to what is being tested
+#   key: the key of the field that is being tested
+#   expected value: the value that the field should contain
+test_repo_info () {
+        label=$1
+        init_command=$2
+        key=$3
+        expected_value=$4
+
+        test_expect_success PERLJSON "json: $label" '
+		test_when_finished "rm -rf repo" &&
+		eval "$init_command" &&
+		echo "$expected_value" >expect &&
+		git -C repo repo-info "$key" >output &&
+		parse_json <output >parsed &&
+		grep -F "row[0].$key" parsed | cut -d " " -f 2 >value &&
+		sed -n -e "/row[0].$key/{
+			s/^[^ ]* //
+			s/^1\$/true/
+			s/^0\$/false/
+			p;
+			}" parsed >actual &&
+		sed "s/^0$/false/" <value| sed "s/^1$/true/" >actual &&
+		test_cmp expect actual
+        '
+
+        test_expect_success "null-terminated: $label" '
+		test_when_finished "rm -rf repo" &&
+		eval "$init_command" &&
+		echo "$expected_value" | lf_to_nul >expect &&
+		git -C repo repo-info --format=null-terminated "$key" >output &&
+		tail -n 1 output >actual &&
+		test_cmp expect actual
+	'
+}
+
+test_repo_info 'ref format files is retrieved correctly' '
+	git init --ref-format=files repo' 'references.format' 'files'
+
+test_repo_info 'ref format reftable is retrieved correctly' '
+	git init --ref-format=reftable repo' 'references.format' 'reftable'
+
+test_done
