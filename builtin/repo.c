60494d5a17cdbe5af1d939dc5a51c62b58ba6734
Lucas Seiki Oshiro <lucasseikioshiro@gmail.com>
2025-07-14 12:17:32 -0300

    repo: add the field references.format
    This commit is part of the series that introduce the new subcommand
    git-repo-info.
    
    The flag `--show-ref-format` from git-rev-parse is used for retrieving
    the reference format (i.e. `files` or `reftable`). This way, it is
    used for querying repository metadata, fitting in the purpose of
    git-repo-info.
    
    Then, add a new field `references.format` to the repo-info subcommand
    containing that information.
    
    Helped-by: Phillip Wood <phillip.wood@dunelm.org.uk>
    Helped-by: Junio C Hamano <gitster@pobox.com>
    Helped-by: Justin Tobler <jltobler@gmail.com>
    Mentored-by: Karthik Nayak <karthik.188@gmail.com>
    Mentored-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Lucas Seiki Oshiro <lucasseikioshiro@gmail.com>

--- builtin/repo.c
+++ builtin/repo.c
@@ -1,11 +1,95 @@
 #include "builtin.h"
 #include "parse-options.h"
+#include "strbuf.h"
+#include "refs.h"
 
-static int repo_info(int argc UNUSED,
-		     const char **argv UNUSED,
-		     const char *prefix UNUSED,
-		     struct repository *repo UNUSED)
+typedef void add_field_fn(struct strbuf *buf, struct repository *repo);
+
+struct field {
+	const char *key;
+	add_field_fn *add_field_callback;
+};
+
+static void add_string(struct strbuf *buf,
+		       const char *key, const char *value)
 {
+	strbuf_addf(buf, "%s\n%s%c", key, value, '\0');
+}
+
+static void add_references_format(struct strbuf *buf,
+				  struct repository *repo)
+{
+	add_string(buf, "references.format",
+		   ref_storage_format_to_name(repo->ref_storage_format));
+}
+
+// repo_info_fields keys should be in lexicographical order
+static const struct field repo_info_fields[] = {
+	{"references.format", add_references_format},
+};
+
+static int repo_info_fields_cmp(const void *va, const void *vb)
+{
+	const struct field *a = va;
+	const struct field *b = vb;
+
+	return strcmp(a->key, b->key);
+}
+
+static add_field_fn *get_append_callback(const char *key) {
+	const struct field search_key = {key, NULL};
+	const struct field *found = bsearch(&search_key, repo_info_fields,
+					    ARRAY_SIZE(repo_info_fields),
+					    sizeof(struct field),
+					    repo_info_fields_cmp);
+	return found ? found->add_field_callback : NULL;
+}
+
+static int qsort_strcmp(const void *va, const void *vb)
+{
+	const char *a = *(const char **)va;
+	const char *b = *(const char **)vb;
+
+	return strcmp(a, b);
+}
+
+static void print_fields(int argc, const char **argv, struct repository *repo) {
+	const char *last = "";
+	struct strbuf buf;
+	strbuf_init(&buf, 256);
+
+	QSORT(argv, argc, qsort_strcmp);
+
+	for (int i = 0; i < argc; i++) {
+		add_field_fn *callback;
+		const char *key = argv[i];
+
+		if (!strcmp(key, last))
+			continue;
+
+		callback = get_append_callback(key);
+
+		if (!callback) {
+			error("key %s not found", key);
+			strbuf_release(&buf);
+			exit(1);
+		}
+
+		callback(&buf, repo);
+		last = key;
+	}
+
+	fwrite(buf.buf, 1, buf.len, stdout);
+	strbuf_release(&buf);
+}
+
+static int repo_info(int argc,
+		     const char **argv,
+		     const char *prefix UNUSED,
+		     struct repository *repo)
+{
+
+	print_fields(argc - 1 , argv + 1, repo);
 	return 0;
 }
 
@@ -16,7 +100,7 @@ int cmd_repo(int argc,
 {
 	parse_opt_subcommand_fn *fn = NULL;
 	const char *const repo_usage[] = {
-		"git repo info",
+		"git repo info [<key>...]",
 		NULL
 	};
 	struct option options[] = {
---
+++ t/t1900-repo.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+test_description='test git repo-info'
+
+. ./test-lib.sh
+
+# Test if a field is correctly returned in the null-terminated format
+#
+# Usage: test_repo_info <label> <init command> <key> <expected value>
+#
+# Arguments:
+#   label: the label of the test
+#   init command: a command that creates a repository called 'repo', configured
+#      accordingly to what is being tested
+#   key: the key of the field that is being tested
+#   expected value: the value that the field should contain
+test_repo_info () {
+	label=$1
+	init_command=$2
+	key=$3
+	expected_value=$4
+
+	test_expect_success "$label" '
+		test_when_finished "rm -rf repo" &&
+		eval "$init_command" &&
+		echo "$expected_value" | lf_to_nul >expected &&
+		git -C repo repo info "$key" >output &&
+		tail -n 1 output >actual &&
+		test_cmp expected actual
+	'
+}
+
+test_repo_info 'ref format files is retrieved correctly' '
+	git init --ref-format=files repo' 'references.format' 'files'
+
+test_repo_info 'ref format reftable is retrieved correctly' '
+	git init --ref-format=reftable repo' 'references.format' 'reftable'
+
+test_expect_success "only one value is returned if the same key is requested twice" '
+	echo "references.format" > expected &&
+	git rev-parse --show-ref-format > ref-format &&
+	lf_to_nul <ref-format >>expected &&
+	git repo info references.format references.format > actual &&
+	test_cmp expected actual
+'
+
+test_done
