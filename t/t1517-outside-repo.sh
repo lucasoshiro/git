cea6c2e77d73c4ac70c5311cb92180b6a4947c3e
Usman Akinyemi <usmanakinyemi202@gmail.com>
2025-08-03 07:37:43 +0530

    t5200: move `update-server-info -h` test from t1517
    t1517 is now focused on testing subcommands outside a repository.
    Move the in-repo `-h` test for `update-server-info` to t5200,
    which covers this command.
    
    Suggested-by: Patrick Steinhardt <ps@pks.im>
    Helped-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Usman Akinyemi <usmanakinyemi202@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t1517-outside-repo.sh
+++ t/t1517-outside-repo.sh
@@ -107,17 +107,13 @@ test_expect_success LIBCURL 'remote-http outside repository' '
 	test_grep "^error: remote-curl" actual
 '
 
-test_expect_success 'update-server-info does not crash with -h' '
-	test_expect_code 129 git update-server-info -h >usage &&
-	test_grep "[Uu]sage: git update-server-info " usage
-'
-
 test_expect_success 'prune does not crash with -h' '
 	test_expect_code 129 git prune -h >usage &&
 	test_grep "[Uu]sage: git prune " usage &&
 	test_expect_code 129 nongit git prune -h >usage &&
 	test_grep "[Uu]sage: git prune " usage
 '
+
 for cmd in $(git --list-cmds=main)
 do
 	cmd=${cmd%.*} # strip .sh, .perl, etc.
