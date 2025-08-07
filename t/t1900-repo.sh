25cbc71a85ab4e30d93dba3696b63182040728b8
Lucas Seiki Oshiro <lucasseikioshiro@gmail.com>
2025-08-07 17:45:39 -0300

    repo: add the field path.toplevel
    The flag `--show-toplevel` from git-rev-parse is used for retrieving
    the top level directory path of the repository. This way, it is used for
    querying repository metadata, fitting in the purpose of git-repo-info.
    
    Then, add a new field `path.toplevel` to the git-repo-info subcommand
    containing that information.
    
    Mentored-by: Karthik Nayak <karthik.188@gmail.com>
    Mentored-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Lucas Seiki Oshiro <lucasseikioshiro@gmail.com>

--- t/t1900-repo.sh
+++ t/t1900-repo.sh
@@ -38,6 +38,53 @@ test_repo_info () {
 	'
 }
 
+test_repo_info_path () {
+	label=$1
+	repo_name=$2
+	key=$3
+	relative_path=$4
+	default=$5
+
+	absolute_path=$(cd "$relative_path"; pwd)/"$repo_name"
+
+	case $default in
+	absolute)
+		expected_value="$absolute_path"
+		;;
+	relative)
+		expected_value="$relative_path"
+		;;
+	esac
+
+	test_expect_success "setup: $label" '
+		git init "$repo_name"
+	'
+
+	test_expect_success "nul: $label" '
+		printf "%s\n%s\0" "$key" "$expected_value" >expected &&
+		git -C "$repo_name" repo info --format=nul "$key" >actual &&
+		test_cmp_bin expected actual
+	'
+
+	test_expect_success "default: $label" '
+		echo "$key=$expected_value" > expected &&
+		git -C "$repo_name" repo info "$key" >actual &&
+		test_cmp_bin expected actual
+	'
+
+	test_expect_success "absolute: $label" '
+		echo "$key=$absolute_path" > expected &&
+		git -C "$repo_name" repo info --path-format=absolute "$key" >actual &&
+		test_cmp_bin expected actual
+	'
+
+	test_expect_success "relative: $label" '
+		echo "$key=$relative_path" > expected &&
+		git -C "$repo_name" repo info --path-format=relative "$key" >actual &&
+		test_cmp_bin expected actual
+	'
+}
+
 test_repo_info 'ref format files is retrieved correctly' \
 	'git init --ref-format=files' 'format-files' 'references.format' 'files'
 
@@ -62,6 +109,21 @@ test_expect_success 'setup remote' '
 
 test_repo_info 'shallow repository = true is retrieved correctly' \
 	'git clone --depth 1 "file://$PWD/remote"' 'shallow' 'layout.shallow' 'true'
+
+test_repo_info_path 'toplevel is retrieved correctly' \
+	'toplevel' 'path.toplevel' './' 'absolute'
+
+test_expect_success 'git-repo-info fails if an invalid key is requested' '
+	echo "error: key ${SQ}foo${SQ} not found" >expected_err &&
+	test_must_fail git repo info foo 2>actual_err &&
+	test_cmp expected_err actual_err
+'
+
+test_expect_success 'git-repo-info outputs data even if there is an invalid field' '
+	echo "references.format=$(test_detect_ref_format)" >expected &&
+	test_must_fail git repo info foo references.format bar >actual &&
+	test_cmp expected actual
+'
 
 test_expect_success 'values returned in order requested' '
 	cat >expect <<-\EOF &&
@@ -98,6 +160,5 @@ test_expect_success 'git-repo-info aborts when requesting an invalid path format
 	test_must_fail git repo info --path-format=foo 2>err &&
 	test_cmp expected err
 '
-
 
 test_done
