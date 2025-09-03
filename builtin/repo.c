b6ecdc2c2fe45cd7ddb58ac58a3d3849366252f0
Lucas Seiki Oshiro <lucasseikioshiro@gmail.com>
2025-09-03 11:54:54 -0300

    repo: add --all to git-repo-info
    Add a new flag `--all` to git-repo-info for requesting values for all
    the available keys. By using this flag, the user can retrieve all the
    values instead of searching what are the desired keys for what they
    wants.
    
    Helped-by: Karthik Nayak <karthik.188@gmail.com>
    Helped-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Lucas Seiki Oshiro <lucasseikioshiro@gmail.com>

--- builtin/repo.c
+++ builtin/repo.c
@@ -9,7 +9,7 @@
 #include "shallow.h"
 
 static const char *const repo_usage[] = {
-	"git repo info [--format=(keyvalue|nul)] [-z] [<key>...]",
+	"git repo info [--format=(keyvalue|nul)] [-z] [--all | <key>...]",
 	NULL
 };
 
@@ -124,6 +124,24 @@ static int print_fields(int argc, const char **argv,
 	return ret;
 }
 
+static void print_all_fields(struct repository *repo,
+			     enum output_format format)
+{
+	struct strbuf valbuf = STRBUF_INIT;
+	struct strbuf quotbuf = STRBUF_INIT;
+
+	for (unsigned long i = 0; i < ARRAY_SIZE(repo_info_fields); i++) {
+		struct field field = repo_info_fields[i];
+
+		strbuf_reset(&valbuf);
+		field.get_value(repo, &valbuf);
+		print_field(format, field.key, &valbuf, &quotbuf);
+	}
+
+	strbuf_release(&valbuf);
+	strbuf_release(&quotbuf);
+}
+
 static int parse_format_cb(const struct option *opt,
 			   const char *arg, int unset UNUSED)
 {
@@ -145,6 +163,7 @@ static int repo_info(int argc, const char **argv, const char *prefix,
 		     struct repository *repo)
 {
 	enum output_format format = FORMAT_KEYVALUE;
+	int all_keys = 0;
 	struct option options[] = {
 		OPT_CALLBACK_F(0, "format", &format, N_("format"),
 			       N_("output format"),
@@ -153,10 +172,19 @@ static int repo_info(int argc, const char **argv, const char *prefix,
 			       N_("synonym for --format=nul"),
 			       PARSE_OPT_NONEG | PARSE_OPT_NOARG,
 			       parse_format_cb),
+		OPT_BOOL(0, "all", &all_keys, N_("return all keys")),
 		OPT_END()
 	};
 
 	argc = parse_options(argc, argv, prefix, options, repo_usage, 0);
+
+	if (all_keys) {
+		if (argc)
+			die(_("--all and <key> cannot be used together"));
+
+		print_all_fields(repo, format);
+		return 0;
+	}
 
 	return print_fields(argc, argv, repo, format);
 }
